7f721162f2cc9f5f436ce537a8ef3c71078ae452
Jared Rummler <jared.rummler@gmail.com>
2016-01-23 20:07:52 -0800

    add header info

--- scripts/remount.sh
+++ scripts/remount.sh
@@ -1,39 +1,40 @@
 #!/system/bin/sh
-##########################################################################
-#
-#
-#
-#
+###########################################################
+# Copyright (C) 2016 Jared Rummler
+# Licensed under GPLv3
+# 
+# DESCRIPTION
+#   Remount a file system on Android
+# 
 
-##########################################################################
+###########################################################
 # FUNCTIONS
-##########################################################################
+###########################################################
 
 remount() {
-	local path=${1:-/system}
-	local option=${2:-rw}
-	local result_code=1
-	local oIFS=$IFS
-	# set the internal field seperator to new line
-	IFS="
+  local path=${1:-/system}
+  local option=${2:-rw}
+  local result_code=1
+  local oifs=$IFS
+  IFS="
 "
-	# read /proc/mounts line by line to get the correct device
-	while read line
-	do
-		local mount_point=$(echo $line | cut -d' ' -f2)
-		if [ "$path" == "$mount_point" ]
-		then
-			local device=$(echo $line | cut -d' ' -f1)
-			result_code=$(mount -o remount,$option $device $mount_point)
-			break
-		fi
-	done < /proc/mounts
-	IFS=$oIFS # restore internal field seperator
-	return $result_code
+  # read /proc/mounts line by line to get the correct device
+  while read line
+  do
+    local mount_point=$(echo $line | cut -d' ' -f2)
+    if [ "$path" == "$mount_point" ]
+    then
+      local device=$(echo $line | cut -d' ' -f1)
+      result_code=$(mount -o remount,$option $device $mount_point)
+      break
+    fi
+  done < /proc/mounts
+  IFS=$oifs # restore internal field seperator
+  return $result_code
 }
 
-##########################################################################
+###########################################################
 # MAIN
-##########################################################################
+###########################################################
 
 remount $@
